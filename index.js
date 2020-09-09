74ef5d3ba1f9507fdde69d7ee750c1de8d6e8ca9
Vincent Servio <vincentservio@gmail.com>
2020-09-09 06:29:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+
+class Cat {
+  constructor (name, sex){
+    this.name = name 
+    this.sex = sex
+  }
+  
+   speak () {
+return `${this.name} says meow!`
+
+}
+  
+}
+
+class Dog {
+ constructor(name){
+ this.name = name 
+ }
+ 
+ speak(){
+   return `${this.name} says woof!`
+ }
+}
+
+class Bird {
+  constructor(name,sex){
+    this.name = name
+    this.sex = sex
+  }
+    speak(){
+      if (this.sex === 'female')
+      return `${this.name} says squawk!`
+      else
+    return `It's me! ${this.name}, the parrot!`
+  }
+}
+
+
+
+// describe("bird", () => {
+//   beforeEach(() => {
+//     bird = new Bird("Pablo", "male")
+//     bird2 = new Bird("Mable", "female")
+//   })
+  
+//   it("has a class of Bird", () => {
+//     expect(new Bird()).to.be.an.instanceof(Bird);
+//   })
+//   it("bird.speak() returns 'It's me! [name], the parrot!'", () => {
+//     expect(bird.speak()).to.eq('It\'s me! Pablo, the parrot!')
+//   })
+//   it("bird2.speak() returns 'Squawk!'", () => {
+//     expect(bird2.speak()).to.eq('Mable says squawk!')
+//   })
+// })
