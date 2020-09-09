66c699531f41dd3208a65b672adcb5b2ba51f9e8
Vincent Servio <vincentservio@gmail.com>
2020-09-09 06:15:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,3 +31,22 @@ class Bird {
     return `It's me! ${this.name}, the parrot!`
   }
 }
+
+
+
+// describe("bird", () => {
+//   beforeEach(() => {
+//     bird = new Bird("Pablo", "male")
+//     bird2 = new Bird("Mable", "female")
+//   })
+  
+//   it("has a class of Bird", () => {
+//     expect(new Bird()).to.be.an.instanceof(Bird);
+//   })
+//   it("bird.speak() returns 'It's me! [name], the parrot!'", () => {
+//     expect(bird.speak()).to.eq('It\'s me! Pablo, the parrot!')
+//   })
+//   it("bird2.speak() returns 'Squawk!'", () => {
+//     expect(bird2.speak()).to.eq('Mable says squawk!')
+//   })
+// })
